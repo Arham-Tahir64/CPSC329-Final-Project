7edec1169c9dbfd2305c47af83927448bdc2178b
30204203 <arham.tahir@ucalgary.ca>
2025-04-03 13:12:55 -0600

    Added UI elements for tab navigation

--- src/App.js
+++ src/App.js
@@ -4,8 +4,18 @@ function App() {
   return (
     <div className="App">
       <h1>React Cryptography Toolkit</h1>
-      <p>Welcome! Select a tool from the tabs above.</p>
-      {/* Tabs and content will go here */}
+
+      {/* Tabs will be here */}
+      <div className="tabs">
+        <button>Caesar Cipher</button>
+        <button>Frequency Analysis</button>
+        <button>Established Ciphers</button>
+      </div>
+
+      <div className="tab-content">
+        <p>Welcome! Select a tool from the tabs above.</p>
+        {/* Content here */}
+      </div>
     </div>
   );
 }
